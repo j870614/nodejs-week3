b1501232ccbf3e8a6587231317b15ca701a3c487
Hironaka <j870614@gmail.com>
2023-04-17 15:46:32 +0800

    refactor: 💡 調整 typecript express 初始化環境樣本

--- src/app.ts
+++ src/app.ts
@@ -4,37 +4,10 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-import { indexRouter } from "./routes/index";
-import { usersRouter } from "./routes/users";
-import { handleError } from "./utils/handleError";
-import { handleNotFoundError } from "./utils/handleNotFoundError";
-
+import { indexRouter } from "./routes";
 // **** Variables **** //
 
-/**
- * API setting example
- * process.env.API_BASEURL = "/api"
- * process.env.API_USER_ENDPOINT = "/users"
- * API_USERS_ENDPOINT = /api/users
- */
-const API_BASEURL = String(process.env.API_BASEURL);
-const API_USERS_ENDPOINT = API_BASEURL + String(process.env.API_USERS_ENDPOINT);
-
 const app = express();
-
-// **** Process Error handler **** //
-
-// Record error and stop this process while the service is done.
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught Exception!");
-  console.error(err);
-  process.exit(1);
-});
-
-// Uncaught catch
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled rejection: ", promise, "reason: ", reason);
-});
 
 // **** Setup **** //
 
@@ -43,20 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
+app.use(morgan("dev"));
 
-// Show routes called in console during development
-if (process.env.NODE_ENV === "dev") {
-  app.use(morgan("dev"));
-}
 
 // Add APIs, must be after middleware
-app.use(API_BASEURL, indexRouter);
-app.use(API_USERS_ENDPOINT, usersRouter);
-
-// Add error handlers
-app.use(handleNotFoundError);
-app.use(handleError);
-
+app.use('/', indexRouter);
 // ** Front-End Content ** //
 
 // Set static directory (js and css)
